0bd8478d428f9b6f602af8bef6c8b7d78adcf105
Aaron Brown <41420965+djbrownbear@users.noreply.github.com>
2024-01-27 00:19:40 -0800

    refactor: rm unnecessary jQuery

--- assets/js/util.js
+++ assets/js/util.js
@@ -19,24 +19,6 @@
 		return false;
 	});
 
-	  /**
-   * Intial loading of Order form based on product type
-   */
-  $(document).ready(function () {
-    var cakeOrCupcake = $(".cakeOrCupcakes").find(":selected").val();
-    console.log("hi", cakeOrCupcake);
-    if (cakeOrCupcake === "cake") {
-      $(".cupcakesQuantity").css("display", "none");
-      $(".cakeSize").css("display", "");
-      $(".cupcakesQuantitySelect").val("");
-    } else if (cakeOrCupcake === "cupcakes") {
-      $(".cakeSize").css("display", "none");
-      $(".cupcakesQuantity").css("display", "");
-      $(".cakeSizeSelect").val("");
-    }
-    return false;
-  });
-
 	/** Clear Form After Submission */
 	$(window).on('beforeunload', function() {
 		$('form').each(function() {
